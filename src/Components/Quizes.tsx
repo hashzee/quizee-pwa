7772dc2b1f2e8d443421023d969ac6d7dc06cf4e
hashzee <55513359+hashzee@users.noreply.github.com>
2020-08-04 10:53:25 +0500

    Animation fixes

--- src/Components/Quizes.tsx
+++ src/Components/Quizes.tsx
@@ -19,7 +19,7 @@ function Quizes() {
               <CardImg top width="100%" src={q1} alt="Card image cap" />
             </div>
             <CardTitle style={{fontSize: "30px", textAlign: "center"}}>Level 1</CardTitle>
-            <CardText style={{textAlign: "center"}}>Easy difficluty level, for the new lerners! Pass and get your Certificate !</CardText>
+            <CardText style={{textAlign: "center", minHeight:'120px'}}>Easy difficluty level, for the new lerners! Pass and get your Certificate !</CardText>
             <Link to='/easy'><Button inverse color="danger">Start Easy</Button></Link>
           </Card>
         </Col>
@@ -29,7 +29,7 @@ function Quizes() {
               <CardImg top width="100%" src={q2} alt="Card image cap" />
             </div>            
             <CardTitle style={{fontSize: "30px", textAlign: "center"}}>Level 2</CardTitle>
-            <CardText style={{textAlign: "center"}}>Medium level for the intermediate users! Can you earn a certificate ?</CardText>
+            <CardText style={{textAlign: "center", minHeight:'120px'}}>Medium level for the intermediate users! Can you earn a certificate ?</CardText>
             <Link to='/medium'><Button inverse color="danger">Start Medium</Button></Link>
           </Card>
         </Col>
@@ -39,7 +39,7 @@ function Quizes() {
               <CardImg top width="100%" src={q3} alt="Card image cap" />
             </div>            
             <CardTitle style={{fontSize: "30px", textAlign: "center"}}>Level 3</CardTitle>
-            <CardText style={{textAlign: "center"}}>Master level, for the legend. You want a certificate, Huh ?</CardText>
+            <CardText style={{textAlign: "center", minHeight:'120px'}}>Master level, for the legend. You want a certificate, Huh ?</CardText>
             <Link to='/hard'><Button inverse color="danger">Start Hard</Button></Link>
           </Card>
         </Col>
